95e1b02ed5dfff762f274dbf011b60589ff01d0d
Tien-Yu <licoria1@gmail.com>
2022-02-24 15:43:56 +0800

    template: ConfirmAddress added, controller: method modified

--- SpringMC/src/main/java/com/example/springmcdonald/controller/ProductController.java
+++ SpringMC/src/main/java/com/example/springmcdonald/controller/ProductController.java
@@ -30,147 +30,156 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/menu")
 public class ProductController {
+
     @Autowired
     ProductService prodService;
     @Autowired
-    OrderLineService orderLineService;            
-    
-    
+    OrderLineService orderLineService;
+
     /*
         redirect
-    */
+     */
     @GetMapping("")
-    public String homepage(Model m){
+    public String homepage(Model m) {
         return share_menu(m);
     }
-    
+
     @GetMapping("/share")
-    public String share_menu(Model m){
+    public String share_menu(Model m) {
         m.addAttribute("list", prodService.findByCategory("SHARE"));
         return "ProductHome";
     }
-        
+
     @GetMapping("/main_course")
-    public String main_course(Model m){
-        m.addAttribute("list", prodService.findByCategory("MAIN"));               
+    public String main_course(Model m) {
+        m.addAttribute("list", prodService.findByCategory("MAIN"));
         return "Main_Course";
     }
-    
+
     @GetMapping("/sub_course")
-    public String sub_course(Model m){
-        m.addAttribute("list", prodService.findByCategory("SUB"));        
+    public String sub_course(Model m) {
+        m.addAttribute("list", prodService.findByCategory("SUB"));
         return "Sub_Course";
     }
-    
+
     @GetMapping("/drinks")
-    public String drinks(Model m){
+    public String drinks(Model m) {
         m.addAttribute("list", prodService.findByCategory("DRINK"));
         return "Drinks";
     }
-    
+
     @GetMapping("/dessert")
-    public String dessert(Model m){
+    public String dessert(Model m) {
         m.addAttribute("list", prodService.findByCategory("DESSERT"));
         return "Dessert";
     }
-    
+
     /**
      * 加入購物車前的確認(加上網址列參數用來判斷產品數量)
+     *
      * @param id 產品編號
      * @param count 產品數量計算器
      * @param countsp 套餐專用數量計算器
+     * @param session 確認有無地址存在
      * @param m 將產品傳給前端UI
-     * @return 
+     * @return
      */
-    
     @GetMapping("/{id}/confirmInfo")
     public String confirmInfo(@PathVariable("id") int id,
-                              @RequestParam(defaultValue = "0") int count,
-                              @RequestParam(defaultValue = "0") int countsp,
-                              Model m){
-        if(count < 0){
+            @RequestParam(defaultValue = "0") int count,
+            @RequestParam(defaultValue = "0") int countsp,
+            HttpSession session,
+            Model m) {
+        if (count < 0) {
             count = 0;
         }
-        if(countsp < 0){
+        if (countsp < 0) {
             countsp = 0;
+        }
+        if (session.getAttribute("name") == null) {
+            if (session.getAttribute("address") == null) {
+                return "ConfirmAddress";
+            }
         }
 
         Optional<Product> tmpProd = prodService.findById(id);
-        
+
         List<Product> subProd = prodService.findByCategory("SUB");
         List<Product> drinkProd = prodService.findByCategory("DRINK");
-        
-        m.addAttribute("product", tmpProd.isPresent()?tmpProd.get():null);
-        
+
+        m.addAttribute("product", tmpProd.isPresent() ? tmpProd.get() : null);
+
         m.addAttribute("sub", subProd);
         m.addAttribute("drink", drinkProd);
-        
+
         m.addAttribute("count", count);
         m.addAttribute("countsp", countsp);
-      
+
         return "confirmInfo";
     }
-    
-    /**   
+
+    /**
      * @param id 產品編號
      * @param orderLineForm 暫存前端傳回來的資訊
      * @param session 儲存訂單資訊
      * @param m 當數量為0時傳給confirmInfo方法用
-     * @return 
+     * @return
      */
     @PostMapping("/shoppingcart")
-    public String shoppingCart(int id, OrderLineForm orderLineForm, HttpSession session, Model m){        
-        Optional<Product> tmpProd = prodService.findById(id);                
+    public String shoppingCart(int id, OrderLineForm orderLineForm, HttpSession session, Model m) {
+        Optional<Product> tmpProd = prodService.findById(id);
         int price = tmpProd.get().getPrice();
         int count = orderLineForm.getCount();
         int countsp = orderLineForm.getCountsp();
         int amount = count + countsp;
-        
+
         //if the form data amount is 0, redirect to confirmInfo page and show the same product
-        if(amount == 0){
-            return confirmInfo(id, 0, 0, m);
+        if (amount == 0) {
+            return confirmInfo(id, 0, 0, session, m);
         }
-        
+
         orderLineForm.setAmount(amount);
-        orderLineForm.setPurchasePrice(price*count + (price+68)*countsp);
+        orderLineForm.setPurchasePrice(price * count + (price + 68) * countsp);
         orderLineForm.setProduct(tmpProd.get());
-        
+
         //convert OrderLineForm to OrderLine
         OrderLine orderLine = orderLineForm.convertToOrderLine();
-        
+
         //insert record to database
         orderLineService.insert(orderLine);
-        
+
         //get the list of OrderLine, if not exists then create one        
-        List<OrderLine> orderLines = (List)session.getAttribute("orderLines");
-        if(orderLines == null){
+        List<OrderLine> orderLines = (List) session.getAttribute("orderLines");
+        if (orderLines == null) {
             orderLines = new ArrayList();
             session.setAttribute("orderLines", orderLines);
-        }        
+        }
         //add orderLine to the orderLine list
         orderLines.add(orderLine);
-               
+
         session.setAttribute("orderLines", orderLines);
         return "ShoppingCart";
     }
+
     /**
      * PostMapping 沒辦法直接透過網址列url存取
+     *
      * @param session
-     * @return 
+     * @return
      */
     @GetMapping("/shoppingcart")
-    public String shoppingCart(HttpSession session){
+    public String shoppingCart(HttpSession session) {
         return "ShoppingCart";
     }
-   
+
     @GetMapping("/{id}/remove")
-    public String removeProduct(@PathVariable int id ,HttpSession session){
-        List<OrderLine> oldList = (List)session.getAttribute("orderLines");
+    public String removeProduct(@PathVariable int id, HttpSession session) {
+        List<OrderLine> oldList = (List) session.getAttribute("orderLines");
         List<OrderLine> newList = oldList.stream().filter(o -> o.getId() != id).collect(Collectors.toList());
         orderLineService.remove(id);
         session.setAttribute("orderLines", newList);
 //        return shoppingCart(session);
         return "redirect:/menu/shoppingcart";
     }
-  
+
 }
--- SpringMC/src/main/java/com/example/springmcdonald/controller/UsersController.java
+++ SpringMC/src/main/java/com/example/springmcdonald/controller/UsersController.java
@@ -21,106 +21,137 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * next : confirmAddress 與 下單時的處裡
+ *
  * @author timothy
  */
 @Controller
 @RequestMapping("/users")
 public class UsersController {
+
     @Autowired
-    UsersService usersService;    
+    UsersService usersService;
+
     /*
         redirect    
-    */       
+     */
     @GetMapping("/account")
-    public String usersHome(){
+    public String usersHome() {
 
         return "UsersHome";
     }
+
     @GetMapping("/register")
-    public String register(Model m){
-        m.addAttribute("usersForm", new UsersForm());        
+    public String register(Model m) {
+        m.addAttribute("usersForm", new UsersForm());
         return "Register";
     }
+
     @GetMapping("/login")
-    public String login(Model m){
+    public String login(Model m) {
         m.addAttribute("loginForm", new LoginForm());
         return "Login";
     }
+
     @GetMapping("/logout")
-    public String logout(HttpSession session){
-        session.invalidate();        
+    public String logout(HttpSession session) {
+        session.invalidate();
         return "redirect:/menu";
     }
+
     @GetMapping("/confirmAddress")
-    public String confirmAddress(){
-        
-        
+    public String confirmAddress() {
         return "ConfirmAddress";
     }
-    
-    
+
     /*
         Post
         spring validation dependencies needed
-    */
-    
+     */
     /**
-     * save user infomation to database
-     * user(usersform) vvalidation required
+     * save user infomation to database user(usersform) vvalidation required
+     *
      * @param usersForm
      * @param br
-     * @return 
+     * @return
      */
     @PostMapping("/registerSave")
-    public String registerSave(@Valid UsersForm usersForm ,BindingResult br){
-        if(!usersForm.matchPassword()){
+    public String registerSave(@Valid UsersForm usersForm, BindingResult br) {
+        if (!usersForm.matchPassword()) {
             br.rejectValue("confirmPassword", "Match", "重複輸入密碼錯誤!");
         }
-        
-        if(br.hasErrors()){
+
+        if (br.hasErrors()) {
             return "Register";
         }
-        
+
         Users tmpUsers = usersForm.convertToUsers();
         usersService.insert(tmpUsers);
-        
+
         return "redirect:/users/login";
     }
-    
+
     /**
      * user(loginform) validation required
+     *
      * @param lg
      * @param br
      * @param session
      * @param m
-     * @return 
+     * @return
      */
     @PostMapping("/loginPost")
-    public String loginPost(@Valid LoginForm lg ,BindingResult br, HttpSession session, Model m){
+    public String loginPost(@Valid LoginForm lg, BindingResult br, HttpSession session, Model m) {
 //        m.addAttribute(loginForm); O
 //        m.addAttribute("loginform", loginForm); X Model 的物件中只有以駱駝式命名的名稱才能取得BindingResult中的錯誤資訊
         m.addAttribute("loginForm", lg);
-        if(br.hasErrors()){
+        if (br.hasErrors()) {
             return "Login";
         }
-        
+
         String name = lg.getUserName();
         String password = lg.getPassword();
         Optional<Users> tmpUser = usersService.findByNameAndPassword(name, password);
-        if(tmpUser.isPresent()){
-            session.setAttribute("name", name);            
+        if (tmpUser.isPresent()) {
+            session.setAttribute("name", name);
             return "redirect:/menu";
         }
-        
+
         br.rejectValue("userName", "NoMatch", "帳號可能不正確");
         br.rejectValue("password", "NoMatch", "密碼可能不正確");
         return "Login";
     }
-    
-    
-    
-    
-    
-    
-    
+
+    //最後需要從ProductController中處理下單時確認有無地址的動作
+    /**
+     * session 中要加入完整的address
+     *
+     * @param address
+     * @param floor
+     * @param room
+     * @param comment
+     * @param session
+     * @return
+     */
+    @PostMapping("/addressPost")
+    public String addressPost(String address, String floor, String room, String comment, HttpSession session) {
+        if (address.length() < 10) {
+            return "ConfirmAddress";
+        }
+
+        if (!floor.equals("")) {
+            address = address.concat(floor+"樓");
+        }
+        if (!room.equals("")) {
+            address = address.concat(room+"號房");
+        }
+
+        session.setAttribute("address", address);
+
+        if (!comment.equals("")) {
+            session.setAttribute("comment", comment);
+        }
+
+        return "redirect:/menu";
+    }
+
 }
